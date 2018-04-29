ed9a4d5be3a95a78d14e5a2a4bdebf5a14180964
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-04-29 16:53:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,11 @@ def summon_captain_planet(planeteers)# code an argument here
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)# code an argument here
+  calls.each do |call|
+    ('').size > 4
+  else 
+    false
 end
 
 def find_the_cheese# code an argument here
